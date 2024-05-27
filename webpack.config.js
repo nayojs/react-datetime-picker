efffaef5967bab483c6a4674c7969ca0b8232493
RWUBAKWANAYO <rwubakwanayoolivier@gmail.com>
2024-05-27 23:36:41 +0200

    Chore: Setup project & configure dependencies

---
+++ webpack.config.js
@@ -0,0 +1,29 @@
+const path = require("path");
+
+module.exports = {
+  entry: "./src/index.tsx",
+  output: {
+    filename: "index.js",
+    path: path.resolve(__dirname, "dist"),
+    library: "@nayojs/react-datetime-picker",
+    libraryTarget: "umd",
+    umdNamedDefine: true,
+  },
+  resolve: {
+    extensions: [".tsx", ".ts", ".js"],
+  },
+  module: {
+    rules: [
+      {
+        test: /\.tsx?$/,
+        use: "ts-loader",
+        exclude: /node_modules/,
+      },
+    ],
+  },
+  externals: {
+    react: "react",
+    "react-dom": "react-dom",
+  },
+  mode: "development",
+};
